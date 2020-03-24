4d9fef744ba7c79e16e22f3ee599c8025a416ada
Iqbal Muhammad <dibaliqaja@gmail.com>
2020-03-24 10:07:32 +0700

    show data

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,40 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+import IndexComponent from './components/posts/Index.vue';
+import CreateComponent from './components/posts/Create.vue';
+import EditComponent from './components/posts/Edit.vue';
+
+const routes = [
+    {
+        name: 'posts',
+        path: '/',
+        component: IndexComponent
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateComponent
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditComponent
+    }
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
